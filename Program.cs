f50e08c58664cd08608ee8dc37e33ecb7e1257c1
Kazuroni <sayajinbr30@gmail.com>
2021-08-14 19:39:33 -0300

    Finaliza

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Keri o tsukeyo ze,Yagami?!");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Kisama no shio mo,motte na!");
+            Console.ReadKey();
+            Console.WriteLine("");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Round 1");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ready?");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Go");   
+        }
+    }
+}
